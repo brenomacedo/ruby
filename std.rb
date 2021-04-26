d4cb17f0299d1ce62d299a6bf4f203cfb275b492
Breno macedo <b88660501@gmail.com>
2021-04-26 22:44:01 +0000

    parsing

---
+++ std.rb
@@ -0,0 +1,11 @@
+# puts 'Digite seu nome:'
+# nome = gets
+# printf('O seu nome é %s', nome)
+# puts nome.inspect # nome.chomp to remove the \n
+
+puts 'SOMA'
+puts 'DIGITE DOIS NUMEROS PARA SOMAR'
+x = gets.to_i
+y = gets.to_i
+
+puts x+y
